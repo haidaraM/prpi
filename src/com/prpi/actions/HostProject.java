54044370ae294442fee84df30bf79b0464d5e2e7
David-Wobrock <david.wobrock@gmail.com>
2016-03-03 15:48:26 +0100

    try configurating log4j properties file

--- src/com/prpi/actions/HostProject.java
+++ src/com/prpi/actions/HostProject.java
@@ -2,26 +2,36 @@ package com.prpi.actions;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
+import com.intellij.util.lang.UrlClassLoader;
 import com.prpi.network.PrPiServerThread;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.helpers.Loader;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class HostProject extends AnAction {
+    private static final Logger logger = LogManager.getLogger(HostProject.class);
+
     private Thread serverThread;
     private static final int DEFAULT_PORT = 4211;
 
     @Override
     public void actionPerformed(AnActionEvent anActionEvent) {
-        System.out.println("ACTION PERFORMED");
-        Project project = anActionEvent.getProject();
+        URL s1 = UrlClassLoader.getSystemResource("config/log4j.xml");
+        URL s2 = Loader.getResource("config/log4j.xml");
+        URL s3 = ClassLoader.getSystemResource("config/log4j.xml");
 
-        try {
-            launchServerInThread(DEFAULT_PORT);
-        } catch (IOException ex) {
-            Messages.showErrorDialog(project, ex.getMessage(), "Error Starting Server Thread");
-        }
+        System.out.println("-------------");
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+        PropertyConfigurator.configure("config/log4j.xml");
+
+        logger.debug("Host projection action performed ended");
     }
 
     private void launchServerInThread(int port) throws IOException {
