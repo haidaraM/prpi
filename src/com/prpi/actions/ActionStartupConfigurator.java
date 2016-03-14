1679043a1380afb1d7804e9e213fd85d27c80d48
David-Wobrock <david.wobrock@gmail.com>
2016-03-14 20:05:09 +0100

    my readme was in the wrong folder --'

---
+++ src/com/prpi/actions/ActionStartupConfigurator.java
@@ -0,0 +1,33 @@
+package com.prpi.actions;
+
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.components.ApplicationComponent;
+import org.apache.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
+
+public class ActionStartupConfigurator implements ApplicationComponent {
+    private static final Logger logger = Logger.getLogger(ActionStartupConfigurator.class);
+
+    @Override
+    public void initComponent() {
+        logger.debug("Init component");
+
+        ActionManager actionManager = ActionManager.getInstance();
+        // Look in configuration if this module is a SHARED_MODULE
+
+        // if no, disable Edit shared project configurator
+
+        logger.debug("End of component init");
+    }
+
+    @Override
+    public void disposeComponent() {
+        logger.debug("Dispose component");
+    }
+
+    @NotNull
+    @Override
+    public String getComponentName() {
+        return "ActionStartupConfigurator";
+    }
+}
