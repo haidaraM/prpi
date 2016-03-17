26d3675a0bde26c2413d47b69d99799e09ca2bb5
Antoine Chabert <chabert.antoine@gmail.com>
2016-03-17 23:16:56 +0100

    Fix log4j configuration file - Add the path in VM option is not needed anymore

--- README.md
+++ README.md
@@ -5,9 +5,11 @@ Projet Réseau - Plugin IntelliJ
 
 ## How to suppress log4j configuration warning on IntelliJ startup
 
-For IntelliJ :
+!! This section is not needed anymore - Skip it !!
 
-Run > Edit Configurations
-In the run configuration for the PrPi module, add in VM Options :
-* `-Dlog4j.configuration=file:C:\Users\David\Documents\INSA\4IF\ReseauSecurite\prpi\resources\log4j.xml` for Windows
-* `-Dlog4j.configuration=file:/home/david/Documents/INSA/4IF/ReseauSecurite/prpi/resources/log4j.xml` for a UNIX System
+~~For IntelliJ :~~
+
+~~Run > Edit Configurations~~
+~~In the run configuration for the PrPi module, add in VM Options :~~
+* ~~`-Dlog4j.configuration=file:C:\Users\David\Documents\INSA\4IF\ReseauSecurite\prpi\resources\log4j.xml` for Windows~~
+* ~~`-Dlog4j.configuration=file:/home/david/Documents/INSA/4IF/ReseauSecurite/prpi/resources/log4j.xml` for a UNIX System~~
--- src/com/prpi/actions/ActionStartupConfigurator.java
+++ src/com/prpi/actions/ActionStartupConfigurator.java
@@ -4,17 +4,24 @@ import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.components.ApplicationComponent;
 import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class ActionStartupConfigurator implements ApplicationComponent {
     private static final Logger logger = Logger.getLogger(ActionStartupConfigurator.class);
 
     @Override
     public void initComponent() {
+        // Set the log4j configuration file
+        DOMConfigurator.configure(getClass().getClassLoader().getResource("/log4j.xml"));
+
         logger.debug("Init component");
 
-        String type = PropertiesComponent.getInstance().getValue("type");
-        logger.debug("Type : " + type);
+        logger.debug(PropertiesComponent.getInstance().getValue("version"));
         ActionManager actionManager = ActionManager.getInstance();
         // Look in configuration if this module is a SHARED_MODULE
 
