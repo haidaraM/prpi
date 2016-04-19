74682c83c32fb2750d0b88c6cf0d77f13ea86cf2
Antoine Chabert <chabert.antoine@gmail.com>
2016-04-19 17:41:52 +0200

    WIP - Change the validation method to show a progress bar when projet files are download

--- src/com/prpi/wizard/JoinProjectBuilder.java
+++ src/com/prpi/wizard/JoinProjectBuilder.java
@@ -7,10 +7,15 @@ import com.intellij.openapi.module.ModuleType;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ModifiableRootModel;
 import com.intellij.openapi.roots.ui.configuration.ModulesProvider;
+import com.intellij.openapi.ui.DialogBuilder;
+import com.intellij.openapi.ui.DialogWrapper;
 import com.prpi.ProjectComponent;
 import com.prpi.network.client.Client;
 import org.apache.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class JoinProjectBuilder extends ExistingModuleLoader {
 
@@ -40,26 +45,65 @@ public class JoinProjectBuilder extends ExistingModuleLoader {
 
     @Override
     public boolean validate(final Project current, final Project dest) {
-        ProjectComponent projectApp = dest.getComponent(ProjectComponent.class);
-        Client client = new Client(dest);
-        projectApp.setClient(client);
-        logger.debug("Begin init client");
-
-        client.connect(hostname, port);
-
-        try {
-            client.downloadProjetFiles();
-        } catch (InterruptedException e) {
-            logger.error(e);
-        }
-        // TODO Disable last wizard step and set project name and properties (not with iml like this...)
-        // return super.validate(current, dest);
-        return false;
+        return this.initClientAndProject(dest) && super.validate(current, dest);
     }
 
     public void setHostnameAndPort(String hostname, int port) {
         this.hostname = hostname;
         this.port = port;
+    }
+
+    private boolean initClientAndProject(final Project dest) {
+
+        ProjectComponent projectApp = dest.getComponent(ProjectComponent.class);
+        Client client = new Client(dest);
+        projectApp.setClient(client);
+        logger.debug("Begin init client");
+
+        if (client.connect(hostname, port)) {
+            try {
+                client.downloadProjetFiles();
+            } catch (InterruptedException e) {
+                logger.error(e);
+            }
+
+
+            DialogBuilder dialogBuilder = new DialogBuilder(dest);
+            dialogBuilder.removeAllActions();
+            dialogBuilder.addCancelAction();
+            dialogBuilder.setTitle("Download project files");
+
+            JPanel dialogPanel = new JPanel();
+            dialogPanel.setLayout(new BoxLayout(dialogPanel, BoxLayout.Y_AXIS));
+
+            final int progressBarMaxValue = 100;
+            JProgressBar progressBar = new JProgressBar(0, progressBarMaxValue);
+            progressBar.setValue(50);
+            dialogPanel.add(progressBar);
+
+            dialogPanel.add(new JLabel("Downloading files from the remote project ..."));
+
+            Runnable progressTask = new Runnable() {
+                public void run() {
+                    while (progressBar.getValue() < progressBarMaxValue) {
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        progressBar.setValue(progressBar.getValue()+1);
+                    }
+                }
+            };
+
+            dialogBuilder.setCenterPanel(dialogPanel);
+            //progressTask.run();
+
+            return dialogBuilder.show() == DialogWrapper.OK_EXIT_CODE;
+        } else {
+            logger.error("Client can't connect to the remote server !");
+        }
+        return false;
     }
 }
 
